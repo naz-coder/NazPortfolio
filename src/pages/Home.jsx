993fe022838be436a068be8ed68644aa07099f47
naz-coder <favouritenaz@yahoo.com>
2023-06-30 11:54:51 +0100

    Contact form width adjusted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import PageHero from '../components/hero/PageHero';
 import AboutMe from '../components/about/AboutMe';
 import Services from '../components/services/Services';
 import Portfolio from "../components/portfolio/Portfolio";
-import Loading from "../components/atoms/Loading";
+// import Loading from "../components/atoms/Loading";
 import Footer from '../components/footer/Footer';
 import BackToTop from "../components/atoms/BackToTop";
 
